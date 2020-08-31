fc16bf8181fb302b898a0b11b3e62ea7cee6d723
kyweez <auboudier.pro@gmail.com>
2020-09-01 00:15:56 +0200

    SQL working

---
+++ 00-TRAINING-IAN-SCHOONOVER/03-30-show-table.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE	cat_app;
+USE cat_app;
+CREATE TABLE cats
+(
+name VARCHAR(100),
+age INT
+);
+SHOW TABLES;
+SHOW COLUMNS FROM cats;
---
+++ 00-TRAINING-IAN-SCHOONOVER/03-32-drop-table.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE	cat_app;
+USE cat_app;
+CREATE TABLE cats
+(
+name VARCHAR(100),
+age INT
+);
+SHOW TABLES;
+SHOW COLUMNS FROM cats;
+DROP TABLE cats;
