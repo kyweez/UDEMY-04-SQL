1c50ac85e1f055c7bf4040dc27ae1399abbd12c7
kyweez <auboudier.pro@gmail.com>
2020-09-10 08:33:55 +0200

    update

---
+++ 00-TRAINING-IAN-SCHOONOVER/05-73-update.sql
@@ -0,0 +1,7 @@
+-- CODE: Updating Data
+
+-- Change tabby cats to shorthair:
+UPDATE cats SET breed='Shorthair' WHERE breed='Tabby'; 
+
+-- Another update:
+UPDATE cats SET age=14 WHERE name='Misty'; 
